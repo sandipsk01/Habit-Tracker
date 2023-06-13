d82558ac58d3fedc324f845e11de1aa43004d7c7
“Sandip <sandipkalekar808@gmail.com>
2023-06-13 12:52:50 +0530

    Habit Tracker Project Complete

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// storing the db on mongo atlas
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
